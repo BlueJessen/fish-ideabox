39312912d14d21dea7093fd4e5730e49488d1b80
Blue Jessen <canealisblue@gmail.com>
2022-07-06 13:39:50 -0500

    Initial Commit

---
+++ src/FishData.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import './FishData.css'
+
+class FishData extends Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+
+  render() {
+
+  }
+}
+export default FishData
